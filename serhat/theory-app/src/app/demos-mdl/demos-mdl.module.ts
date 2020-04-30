bb271c753f13e42b6972abf8a43eb71931b02b9a
serhat <serhat@test.com>
2020-04-30 11:43:02 +0200

    Events & Outputs

--- serhat/theory-app/src/app/demos-mdl/demos-mdl.module.ts
+++ serhat/theory-app/src/app/demos-mdl/demos-mdl.module.ts
@@ -5,11 +5,23 @@ import { CardComponent } from './card/card.component';
 import { DemoOverviewComponent } from './demo-overview/demo-overview.component';
 import { FurnitureComponent } from './furniture/furniture.component';
 import { RomanNumberComponent } from './roman-number/roman-number.component';
+import { PipesComponent } from './pipes/pipes.component';
+import { EventsComponent } from './events/events.component';
+import { OutputsComponent } from './outputs/outputs.component';
+import { NgformComponent } from './ngform/ngform.component';
 
 
 
 @NgModule({
-  declarations: [TimeComponent, CardComponent, DemoOverviewComponent, FurnitureComponent, RomanNumberComponent],
+  declarations: [TimeComponent,
+    CardComponent,
+    DemoOverviewComponent,
+    FurnitureComponent,
+    RomanNumberComponent,
+    PipesComponent,
+    EventsComponent,
+    OutputsComponent,
+    NgformComponent],
   imports: [
     CommonModule
   ],
